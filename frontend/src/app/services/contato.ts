00baa34dfb060cfa1fdf3229902d0fde9181b9e3
LuisLaporta <lui.laporta99@gmail.com>
2025-07-18 02:24:42 -0300

    feat(frontend): criando service de contato

--- frontend/src/app/services/contato.ts
+++ frontend/src/app/services/contato.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Contato {
+
+export class ContatoService {
+  private API_URL = "http://localhost:8000/api.php";
   
+  constructor(private httpClinet: HttpClient) {}
+
+  getContatos(): Observable<Object> {
+    return this.httpClinet.get(this.API_URL);
+  }
+
 }
