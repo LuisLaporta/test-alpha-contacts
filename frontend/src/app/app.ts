80423dfcffdffe521f122902bb925a5073e87e0d
Luis Laporta <lui.laporta99@gmail.com>
2025-07-20 20:41:49 -0300

    feat(frontend): formsmodule

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,9 @@
 import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
