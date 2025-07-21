19146871408db23da5a6187ce306c46d6eeaa3ba
Luis Laporta <lui.laporta99@gmail.com>
2025-07-21 11:03:54 -0300

    feat(style): estilizando o componente form

--- frontend/src/app/components/button/button.ts
+++ frontend/src/app/components/button/button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './button.css'
 })
 export class Button {
-
+  @Input() isEditing: boolean = false;
+  @Input() disabled: boolean = false;
 }
